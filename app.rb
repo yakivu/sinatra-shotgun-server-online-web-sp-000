a094df6b24bf4b3a0b34a0e1a0c1cd5f6e9d16a6
Adam Walker <adamw207@yahoo.com>
2021-04-07 17:18:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,11 @@
+
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Started my server using Shotgun!"
   end
 
 end
